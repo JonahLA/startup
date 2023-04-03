36f9c6a0a1d1bbbdb22e469e88f58c76b111ac2d
Jonah <jonahaustin@outlook.com>
2023-04-02 20:06:35 -0600

    update(login.js) refactor from class to functional approach

--- public/login.js
+++ public/login.js
@@ -1,48 +1,52 @@
 
-class AuthenticationHandler {
-    requiredFields;
-    submissionButtons;
+async function login() {
+    this.loginOrRegister('login');
+}
 
-    constructor() {
-        this.requiredFields = document.querySelectorAll("input.required-field");
-        this.submissionButtons = document.querySelectorAll("button.submission-button"); 
-    }
+async function register() {
+    this.loginOrRegister('register');
+}
 
-    // TODO: when we hook up a database, actually figure this out
-    login() {
-        // Get the content from the username field and save it to local storage
-        const usernameElement = document.querySelector('#username-field');
-        const username = usernameElement.value;
-        localStorage.setItem('dvd-game-username', username);
-        window.location.href = "lobby.html";
-    }
+async function loginOrRegister(endpoint) {
+    // Get the content from the username field and save it to local storage
+    const username = document.querySelector('#username-field')?.value;
+    const password = document.querySelector('#password-field')?.value;
+    const user = { username: username, password: password };
 
-    // TODO: when we hook up a database, actually figure this out
-    register() {
-        // For the time being, this is going to do the same thing as login
-        this.login();
-    }
+    // Construct an HTTP POST login request and send it to the server
+    const response = await fetch(`/api/user/${endpoint}`, {
+        method: 'POST',
+        body: JSON.stringify(user),
+        headers: { 'Content-Type': 'application/json; charset=UTF-8', }
+    })
+    const body = await response.json();
 
-    checkCompletion(inputElement) {
-        // FOR DEBUGGING: console.log(`This element changed: ${inputElement.id}`);
-
-        // Determine if the fields are completed or not
-        let isComplete = true;
-        this.requiredFields.forEach((fieldElement) => {
-            if (fieldElement.value === "") {
-                isComplete = false;
-            }
-        });
-
-        // Enable the button if the fields are completed; otherwise, disable them
-        this.submissionButtons.forEach((buttonElement) => {
-            if (isComplete) {
-                buttonElement.removeAttribute('disabled');
-            } else {
-                buttonElement.setAttribute('disabled', '');    
-            }
-        });
+    if (response?.status === 200) {
+        localStorage.setItem("username", username);
+        window.location.href = 'lobby.html';
+    } else {
+        // SHOW ERROR MESSAGE
     }
 }
 
-const authenticationHandler = new AuthenticationHandler();
+function checkCompletion() {
+    requiredFields = document.querySelectorAll("input.required-field");
+    submissionButtons = document.querySelectorAll("button.submission-button"); 
+
+    // Determine if the fields are completed or not
+    let isComplete = true;
+    this.requiredFields.forEach((fieldElement) => {
+        if (fieldElement.value === "") {
+            isComplete = false;
+        }
+    });
+
+    // Enable the button if the fields are completed; otherwise, disable them
+    this.submissionButtons.forEach((buttonElement) => {
+        if (isComplete) {
+            buttonElement.removeAttribute('disabled');
+        } else {
+            buttonElement.setAttribute('disabled', '');    
+        }
+    });
+}
